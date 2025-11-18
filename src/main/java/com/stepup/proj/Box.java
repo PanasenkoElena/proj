c11234a04eb127a426d008bab29f819611e4996f
Elena Panaseko <git.user.panasenko-elen@yandex.ru>
2025-11-18 23:29:08 +0300

    Изменена логика doSmth

--- src/main/java/com/stepup/proj/Box.java
+++ src/main/java/com/stepup/proj/Box.java
@@ -1,4 +1,20 @@
 package com.stepup.proj;
 
 public class Box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    puBlic Box() {
+    }
+
+    public Box(int size) {
+        this.size = size;
+    }
 }
